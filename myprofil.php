b2956682a25018a2a805cdfd09796a74d531e52e
cynthia-saro <cyn.saro@gmail.com>
2018-11-06 16:57:11 +0100

    info mon profil

--- myprofil.php
+++ myprofil.php
@@ -1,15 +1,19 @@
 <?php
   include ("php/db.php");
+  /* marche dans le html
+  <?php echo $_SESSION['member']['id'] ?>
+  */
+  $iduser = $_SESSION['member']['id'];
+  print_r ($iduser);
 ?>
-
 
 <?php include("layouts/header.php");?>
 
 <main class="fond_blur">
   <h1> Mon profil</h1>
   <h2> Mes evenements </h2>
-
   <?php echo $_SESSION['member']['id'] ?>
+
   </div>
 
 </main>
