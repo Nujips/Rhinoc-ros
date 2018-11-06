3c8c3e8a3c961b69054c4630d1c1c516a6860a5d
cynthia-saro <cyn.saro@gmail.com>
2018-11-06 15:16:18 +0100

    header test affichage elem

--- layouts/header.php
+++ layouts/header.php
@@ -1,5 +1,7 @@
 <?php include("layouts/head.php");?>
 <header>
-  <?php session_start();?>
-  <?php include("./php/navbar.php")?>
+  <?php
+  include("./php/navbar.php");
+  session_start();
+  ?>
 </header>
