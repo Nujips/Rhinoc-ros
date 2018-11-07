8372726107eb026574ba268707686feb794eced8
cynthia-saro <cyn.saro@gmail.com>
2018-11-07 10:08:48 +0100

    test requete

--- php/delete_acc.php
+++ php/delete_acc.php
@@ -3,11 +3,12 @@ require("db.php");
 
 $id = $_SESSION['member']['id'];
 
-$sql = "DELETE FROM members, events WHERE members.id = $id AND events.idcreator = $id";
+$sql = "DELETE * FROM members, events WHERE members.id = $id AND events.idcreator = $id";
+$sql = "DELETE * FROM members JOIN events ON members.id = $id AND events.idcreator = $id";
 
 $stmt = $conn->prepare($sql);
 $stmt->bindValue (':id', $id);
 $stmt->execute();
 
-header('Location: index.php');
+header('Location: ../index.php');
 ?>
